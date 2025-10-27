e8868a9126674a1e6e63bf9e900b45dcf53ebd2b
Otutu11 <otutuanslem01@gmail.com>
2025-10-28 00:32:54 +0100

    feat: added temputils functions...

--- src/hooks/use-grid.ts
+++ src/hooks/use-grid.ts
@@ -1,1 +1,14 @@
-export const useGridState = () => {};
+import { useState } from "react";
+import type { IGridActions, IGridState, IMergeInfo } from "@/interface/types";
+
+export const useGridState = (): IGridState & IGridActions => {
+  const [selectedCells, setSelectedCells] = useState<Set<string>>(new Set());
+  const [mergedCells, setMergedCells] = useState<Map<string, IMergeInfo>>(
+    new Map(),
+  );
+  const [hiddenCells, setHiddenCells] = useState<Set<string>>(new Set());
+  const [columnCount, setColumnCount] = useState(12);
+  const [hoveredColumn, setHoveredColumn] = useState<number | null>(null);
+  const [openPopover, setOpenPopover] = useState<number | null>(null);
+  const [editingDuration, setEditingDuration] = useState<number | null>(null);
+};
