ad53f6220970a2bd0d467fb575bf089fdfbad5e3
Otutu11 <otutuanslem01@gmail.com>
2025-10-27 18:31:22 +0100

    Add SectionHeader component and dashboard wiring
    - Ignore the .husky folder in .gitignore
    - Update pre-commit hook to run the config script and adjust messages
    - Add SectionHeader component for the dashboard header
    - Wire SectionHeader into the dashboard layout (index and layout)

--- src/views/layout.tsx
+++ src/views/layout.tsx
@@ -6,6 +6,7 @@ import { SiteHeader } from "@/components/site-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import * as React from "react";
 import data from "./data.json";
+import Dashboard from "./index";
 
 interface LayoutProps {
   propName: type;
@@ -29,11 +30,13 @@ const Layout: React.FC<LayoutProps> = ({ propName }) => {
             <div className="@container/main flex flex-1 flex-col gap-2">
               <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
                 <SectionCards />
-                <div className="px-4 lg:px-6 grid grid-cols-2 gap-4">
-                  <ChartAreaInteractive />
-                  <ChartAreaInteractive />
+                <div className="px-4 lg:px-6 gap-4 h-full w-full">
+                  <Dashboard />
                 </div>
-                <DataTable data={data} />
+                {/*<div className="px-4 lg:px-6">
+                  <ChartAreaInteractive />
+                </div>*/}
+                {/*<DataTable data={data} />*/}
               </div>
             </div>
           </div>
