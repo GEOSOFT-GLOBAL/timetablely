92281ee47d9ee5fc08a0d1f5dbd0b9c3691fe5b7
Otutu11 <otutuanslem01@gmail.com>
2025-11-02 16:29:59 +0100

    feat:Add basic React components for views...

---
+++ src/views/sessions.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+interface SessionsProps {
+  propName?: string;
+}
+
+const Sessions: React.FC<SessionsProps> = () => {
+  return <div></div>;
+};
+
+export default Sessions;
---
+++ src/views/tutors.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+interface TutorsProps {
+  propName?: string;
+}
+
+const Tutors: React.FC<TutorsProps> = () => {
+  return <div></div>;
+};
+
+export default Tutors;
