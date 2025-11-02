e55125b4819e7f1a0b8632a2271c75d642d2ef1c
geodesmond1990-design <geodesmond1990@gmail.com>
2025-11-02 15:54:43 +0100

    refactor: Refactor routing and update dashboard

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,9 +1,13 @@
+import ErrorView from "@/views/error-view";
 import Layout from "@/views/layout";
 import { createHashRouter } from "react-router-dom";
+import { appRoutes } from "./app.routes";
 
 export const routes = createHashRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorView />,
+    children: [...appRoutes],
   },
 ]);
--- src/views/layout.tsx
+++ src/views/layout.tsx
@@ -1,10 +1,8 @@
+import * as React from "react";
+import { Outlet } from "react-router-dom";
 import { AppSidebar } from "@/components/app-sidebar";
-import { SectionCards } from "@/components/section-cards";
 import { SiteHeader } from "@/components/site-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
-import * as React from "react";
-import Dashboard from "./index";
-import TimeTable from "@/components/timetable";
 
 const Layout = () => {
   return (
@@ -22,19 +20,7 @@ const Layout = () => {
           <SiteHeader />
           <div className="flex flex-1 flex-col">
             <div className="@container/main flex flex-1 flex-col gap-2">
-              <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-                <div className="px-4 lg:px-6 gap-4 h-full w-full">
-                  <Dashboard />
-                </div>
-                <div className="flex w-full gap-2 px-6">
-                  <div className="w-3/4">
-                    <TimeTable />
-                  </div>
-                  <div className="w-1/4">
-                    <SectionCards />
-                  </div>
-                </div>
-              </div>
+              <Outlet />
             </div>
           </div>
         </SidebarInset>
