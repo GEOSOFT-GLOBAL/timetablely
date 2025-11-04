6557d4ddc92610902f2179c3978597ce3967567e
Otutu11 <otutuanslem01@gmail.com>
2025-11-04 12:32:58 +0100

    feat: Add labels for input fields...

--- src/views/tutors.tsx
+++ src/views/tutors.tsx
@@ -3,6 +3,7 @@ import TutorItem from "@/components/tutor-item";
 import { Button } from "@/components/ui/button";
 import { Card, CardFooter } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import * as React from "react";
 
 interface TutorsProps {
@@ -18,15 +19,17 @@ const Tutors: React.FC<TutorsProps> = () => {
       <div className=" grid grid-cols-1 w-full gap-4 md:grid-cols-2 md:gap-6">
         <Card className="">
           <div className="p-4 flex flex-col gap-4">
-            <Input />
-            <Input />
+            <Label htmlFor="name">Name</Label>
+            <Input id="name" />
+            <Label htmlFor="email">Email</Label>
+            <Input id="email" />
           </div>
           <CardFooter className="gap-4">
             <Button variant="outline">Save</Button>
             <Button variant="outline">Cancel</Button>
           </CardFooter>
         </Card>
-        <Card className="w-full flex flex-col px-4">
+        <Card className="w-full h-[calc(100vh-220px)] flex flex-col px-4">
           <TutorItem />
           <TutorItem />
           <TutorItem />
